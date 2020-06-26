ee2007ccc85fed6284f6e33b410c70d886a11b76
Vincent Agnano <vinyll@protonmail.com>
2020-06-26 16:56:47 +0200

    switched to form for validating fields

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const main = document.querySelector('main')
 const progress = document.querySelector('progress')
 const previousButton = document.querySelector('a[previous]')
-const nextButton = document.querySelector('a[next]')
+const nextButton = document.querySelector('button[next]')
 const template404 = document.querySelector('template[name="404"]')
 
 const steps = [
@@ -27,7 +27,10 @@ function changePage(name) {
   const step = steps.indexOf(name)
   progress.value = step
   previousButton.onclick = function() {page.redirect(`/${steps[step - 1]}`)}
-  nextButton.onclick = function() {page.redirect(`/${steps[step + 1]}`)}
+  document.forms[0].onsubmit = function(event) {
+    event.preventDefault()
+    page.redirect(`/${steps[step + 1]}`)
+  }
   step <= 0 ? previousButton.setAttribute('disabled', 'disabled') : previousButton.removeAttribute('disabled')
 }
 
